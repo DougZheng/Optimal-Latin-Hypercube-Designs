2cfd66f3b4d3ed425bb34a870f40e4df21573639
DougZheng <739168993@qq.com>
2022-03-11 15:53:45 +0800

    Optimize time cost of Swap and Pow

--- src/ESE_Jin2005.cpp
+++ src/ESE_Jin2005.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <iomanip>
 #include <algorithm>
+#include <utility>
 #include <string>
 #include <vector>
 #include <random>
@@ -18,39 +19,33 @@ class Design {
   static VecInt2D ReadDesign(const std::string& file);
   Design(const VecInt2D& design);
   Design(int n, int k, int seed);
-  inline int GetN() { return n_run_; }
-  inline int GetK() { return k_var_; }
-  inline VecInt2D GetDesign() { return design_; }
-  inline double GetPhiP() { return phi_p_; };
-  inline double GetCritVal(double w, double rho_max) {
-    static auto phi_p_bound = [this]() -> std::pair<double, double> {
-      double d_avg = (n_run_ + 1) * k_var_ / 3.0;
-      double d_floor = std::floor(d_avg);
-      double d_ceil = std::ceil(d_avg);
-      double phi_p_low = std::pow(n_run_ * (n_run_ - 1) / 2 * 
-        ((d_ceil - d_avg) / std::pow(d_floor, kPInPhi) + 
-        (d_avg - d_floor) / std::pow(d_ceil, kPInPhi)), 1.0 / kPInPhi);
-      double phi_p_up = 0;
-      for (int i = 1; i < n_run_; ++i) {
-        phi_p_up += (n_run_ - i) / QuickPow(i * k_var_, kPInPhi);
-      }
-      phi_p_up = std::pow(phi_p_up, 1.0 / kPInPhi);
-      return {phi_p_low, phi_p_up};
-    }();
+  inline int GetN() const { return n_run_; }
+  inline int GetK() const { return k_var_; }
+  inline VecInt2D GetDesign() const { return design_; }
+  inline double GetPhiP() const { return phi_p_; };
+  inline double GetCritVal(double w, double rho_max, double phi_p) {
     return w * rho_max + 
-      (1 - w) * (phi_p_ - phi_p_bound.first) / (phi_p_bound.second - phi_p_bound.first);
+      (1 - w) * (phi_p - phi_p_low_) / (phi_p_up_ - phi_p_low_);
   }
   void SwapInCol(int col, int r1, int r2);
-  double GetMaxAbsCorr(int col);
-  double GetMaxAbsCorr();
-  double GetMaxAbsCorrExcept(int col);
-  void Display();
+  std::pair<double, double> PreSwapInCol(int col, int r1, int r2) const;
+  double GetMaxAbsCorr(int col) const;
+  double GetMaxAbsCorr() const;
+  double GetMaxAbsCorrExcept(int col) const;
+  void Display() const;
  private:
+  inline double QuickPow15(double x) const {
+    double y = x;
+    y *= y, y *= y, y *= y, y *= y;
+    return y / x;
+  }
   void InitCorr();
   void InitDis();
+  void InitPhiPBound();
   void MaintainCorr(int col, int r1, int r2);
   void MaintainDis(int col, int r1, int r2);
-  double QuickPow(double x, int p);
+  double GetPreSwapMaxAbsCorr(int col, int r1, int r2) const;
+  double GetPreSwapPhiP(int col, int r1, int r2) const;
  private:
   const int kPInPhi = 15;
   double kCorrDenominator;
@@ -60,6 +55,8 @@ class Design {
   VecDouble2D corr_;
   VecInt2D dis_;
   double phi_p_;
+  double phi_p_low_;
+  double phi_p_up_;
 };
 
 Design::VecInt2D Design::ReadDesign(const std::string& file) {
@@ -132,10 +129,25 @@ void Design::InitDis() {
         l1_dis += std::abs(design_[i][o] - design_[j][o]);
       }
       dis_[i][j] = dis_[j][i] = l1_dis;
-      phi_p_ += QuickPow(dis_[i][j], -kPInPhi);
+      phi_p_ += 1.0 / QuickPow15(dis_[i][j]);
     }
   }
   phi_p_ = std::pow(phi_p_, 1.0 / kPInPhi);
+  InitPhiPBound();
+}
+
+void Design::InitPhiPBound() {
+  double d_avg = (n_run_ + 1) * k_var_ / 3.0;
+  double d_floor = std::floor(d_avg);
+  double d_ceil = std::ceil(d_avg);
+  phi_p_low_ = std::pow(n_run_ * (n_run_ - 1) / 2 * 
+    ((d_ceil - d_avg) / std::pow(d_floor, kPInPhi) + 
+    (d_avg - d_floor) / std::pow(d_ceil, kPInPhi)), 1.0 / kPInPhi);
+  phi_p_up_ = 0;
+  for (int i = 1; i < n_run_; ++i) {
+    phi_p_up_ += (n_run_ - i) / QuickPow15(i * k_var_);
+  }
+  phi_p_up_ = std::pow(phi_p_up_, 1.0 / kPInPhi);
 }
 
 void Design::SwapInCol(int col, int r1, int r2) {
@@ -144,7 +156,12 @@ void Design::SwapInCol(int col, int r1, int r2) {
   std::swap(design_[r1][col], design_[r2][col]);
 }
 
-double Design::GetMaxAbsCorr(int col) {
+std::pair<double, double> Design::PreSwapInCol(int col, int r1, int r2) const {
+  return std::make_pair(GetPreSwapMaxAbsCorr(col, r1, r2), 
+    GetPreSwapPhiP(col, r1, r2));
+}
+
+double Design::GetMaxAbsCorr(int col) const {
   double max_corr = 0;
   for (int i = 0; i < k_var_; ++i) {
     if (i == col) continue;
@@ -153,7 +170,7 @@ double Design::GetMaxAbsCorr(int col) {
   return max_corr;
 }
 
-double Design::GetMaxAbsCorr() {
+double Design::GetMaxAbsCorr() const {
   double max_corr = 0;
   for (int i = 0; i < k_var_; ++i) {
     for (int j = 0; j < i; ++j) {
@@ -163,7 +180,7 @@ double Design::GetMaxAbsCorr() {
   return max_corr;
 }
 
-double Design::GetMaxAbsCorrExcept(int col) {
+double Design::GetMaxAbsCorrExcept(int col) const {
   double max_corr = 0;
   for (int i = 0; i < k_var_; ++i) {
     if (i == col) continue;
@@ -185,26 +202,52 @@ void Design::MaintainCorr(int col, int r1, int r2) {
 }
 
 void Design::MaintainDis(int col, int r1, int r2) {
-  double phi_p_num = QuickPow(phi_p_, kPInPhi);
+  double phi_p_num = QuickPow15(phi_p_);
   auto UpdateDis = [this, &phi_p_num, col](int r1, int r2, int deta) {
-    if (r1 <= r2) return;
-    phi_p_num -= QuickPow(dis_[r1][r2], -kPInPhi);
+    phi_p_num -= 1.0 / QuickPow15(dis_[r1][r2]);
     dis_[r1][r2] -= std::abs(design_[r2][col] - design_[r1][col]);
     dis_[r1][r2] += std::abs(design_[r2][col] - design_[r1][col] + deta);
-    phi_p_num += QuickPow(dis_[r1][r2], -kPInPhi);
+    phi_p_num += 1.0 / QuickPow15(dis_[r1][r2]);
   };
   int deta = design_[r2][col] - design_[r1][col];
   for (int i = 0; i < n_run_; ++i) {
     if (i == r1 || i == r2) continue;
-    UpdateDis(i, r1, deta);
-    UpdateDis(i, r2, -deta);
-    UpdateDis(r1, i, -deta);
-    UpdateDis(r2, i, deta);
+    i > r1 ? UpdateDis(i, r1, deta) : UpdateDis(r1, i, -deta);
+    i > r2 ? UpdateDis(i, r2, -deta) : UpdateDis(r2, i, deta);
   }
   phi_p_ = std::pow(phi_p_num, 1.0 / kPInPhi);
 }
 
-void Design::Display() {
+double Design::GetPreSwapMaxAbsCorr(int col, int r1, int r2) const {
+  double max_corr = 0;
+  for (int i = 0; i < k_var_; ++i) {
+    if (i == col) continue;
+    int deta = (design_[r1][i] - design_[r2][i]) *
+      (design_[r2][col] - design_[r1][col]);
+    double tmp_corr = (corr_[i][col] * kCorrDenominator + deta) / kCorrDenominator;
+    max_corr = std::max(max_corr, std::fabs(tmp_corr));
+  }
+  return max_corr;
+}
+
+double Design::GetPreSwapPhiP(int col, int r1, int r2) const {
+  double phi_p_num = QuickPow15(phi_p_);
+  auto UpdateDis = [this, &phi_p_num, col](int r1, int r2, int deta) {
+    phi_p_num -= 1.0 / QuickPow15(dis_[r1][r2]);
+    int tmp_dis = dis_[r1][r2] - std::abs(design_[r2][col] - design_[r1][col]) +
+      std::abs(design_[r2][col] - design_[r1][col] + deta);
+    phi_p_num += 1.0 / QuickPow15(tmp_dis);
+  };
+  int deta = design_[r2][col] - design_[r1][col];
+  for (int i = 0; i < n_run_; ++i) {
+    if (i == r1 || i == r2) continue;
+    i > r1 ? UpdateDis(i, r1, deta) : UpdateDis(r1, i, -deta);
+    i > r2 ? UpdateDis(i, r2, -deta) : UpdateDis(r2, i, deta);
+  }
+  return std::pow(phi_p_num, 1.0 / kPInPhi);
+}
+
+void Design::Display() const {
   std::cout << n_run_ << " " << k_var_ << "\n";
   int w = std::floor(std::log10(n_run_)) + 1;
   for (int i = 0; i < n_run_; ++i) {
@@ -214,20 +257,6 @@ void Design::Display() {
   }
   std::cout << "PhiP: " << GetPhiP() << "\n";
   std::cout << "RhoMax: " << GetMaxAbsCorr() << std::endl;
-}
-
-double Design::QuickPow(double x, int p) {
-  bool is_neg = p < 0;
-  p = std::abs(p);
-  double ret = 1;
-  while (p > 0) {
-    if (p & 1) {
-      ret = ret * x;
-    }
-    x = x * x;
-    p >>= 1;
-  }
-  return is_neg ? 1.0 / ret : ret;
 }
 
 class Solver {
@@ -253,9 +282,7 @@ Design Solver::Solve(int n, int k, double w, int limit_cnt) {
 Design Solver::Solve(int n, int k, double w, int limit_cnt, Design design) {
   const int kPrintRatio = 100;
   int n_e = n * (n - 1) / 2;
-  // int J = std::min(50, n_e / 5);
   int J = std::min(50, (n_e + 4) / 5);
-  // int M = std::min(100, 2 * n_e * k / J); // TODO: optimize default parameters
   int M = k;
   double a1 = 0.8;
   double a2 = 0.9;
@@ -264,7 +291,7 @@ Design Solver::Solve(int n, int k, double w, int limit_cnt, Design design) {
   std::uniform_real_distribution<double> uniform_dis(0.0, std::nextafter(1.0, 1.1));
   int iterator_cnt = 0;
   double bst_corr = design.GetMaxAbsCorr();
-  double bst_val = design.GetCritVal(w, bst_corr);
+  double bst_val = design.GetCritVal(w, bst_corr, design.GetPhiP());
   Design::VecInt2D bst_design = design.GetDesign();
   double T_h = 0.005 * bst_val;
   std::vector<std::pair<int, int>> pair_list;
@@ -298,19 +325,18 @@ Design Solver::Solve(int n, int k, double w, int limit_cnt, Design design) {
       for (int j = 0; j < J; ++j) {
         int r1 = pair_list[j].first;
         int r2 = pair_list[j].second;
-        design.SwapInCol(col, r1, r2);
-        double tmp_corr = std::max(corr_except, design.GetMaxAbsCorr(col));
-        double tmp_val = design.GetCritVal(w, tmp_corr);
+        auto tmp_ret = design.PreSwapInCol(col, r1, r2);
+        double tmp_corr = std::max(corr_except, tmp_ret.first);
+        double tmp_val = design.GetCritVal(w, tmp_corr, tmp_ret.second);
         if (tmp_val < inner_bst_val) {
           inner_bst_val = tmp_val;
           inner_bst_corr = tmp_corr;
           bst_r1 = r1;
           bst_r2 = r2;
         }
-        design.SwapInCol(col, r1, r2);
       }
-      design.SwapInCol(col, bst_r1, bst_r2);
       if (inner_bst_val - cur_val <= T_h * uniform_dis(rng_)) {
+        design.SwapInCol(col, bst_r1, bst_r2);
         ++n_acpt;
         if (inner_bst_val < bst_val) {
           bst_val = inner_bst_val;
@@ -319,9 +345,6 @@ Design Solver::Solve(int n, int k, double w, int limit_cnt, Design design) {
         }
         cur_val = inner_bst_val;
         cur_corr = inner_bst_corr;
-      }
-      else {
-        design.SwapInCol(col, bst_r1, bst_r2);
       }
     }
     bool flag_imp = old_val - bst_val > 0;
