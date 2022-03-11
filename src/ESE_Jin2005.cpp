b86cbae1a26613e3e2577250f2576dd4423f0e4c
DougZheng <739168993@qq.com>
2022-03-11 22:44:10 +0800

    Add param setting func

--- src/ESE_Jin2005.cpp
+++ src/ESE_Jin2005.cpp
@@ -197,7 +197,8 @@ void Design::MaintainCorr(int col, int r1, int r2) {
     if (i == col) continue;
     int deta = (design_[r1][i] - design_[r2][i]) *
       (design_[r2][col] - design_[r1][col]);
-    corr_[i][col] = corr_[col][i] = (corr_[i][col] * kCorrDenominator + deta) / kCorrDenominator;
+    corr_[i][col] = corr_[col][i] = 
+      (corr_[i][col] * kCorrDenominator + deta) / kCorrDenominator;
   }
 }
 
@@ -259,129 +260,159 @@ void Design::Display() const {
   std::cout << "RhoMax: " << GetMaxAbsCorr() << std::endl;
 }
 
-class Solver {
+class Searcher {
  public:
-  Solver(int seed = 19937);
-  Design Solve(int n, int k, double w, int limit_cnt);
-  Design Solve(int n, int k, double w, int limit_cnt, Design design);
+  Searcher(int n, int k);
+  inline void SetMCol(int m_col) { m_col_ = m_col; }
+  inline void SetJColPair(int j_col_pair) { j_col_pair_ = j_col_pair; }
+  inline void SetAlpha1(double alpha1) { alpha1_ = alpha1; }
+  inline void SetAlpha2(double alpha2) { alpha2_ = alpha2; }
+  inline void SetAlpha3(double alpha3) { alpha3_ = alpha3; }
+  inline void SetW(double w) { w_ = w; }
+  inline void SetIterateCnt(int iterate_cnt) { iterate_cnt_ = iterate_cnt; }
+  inline void SetSeed(int seed) { rng_.seed(seed); }
+  inline void SetPrintFrequence(int print_frequence) { print_frequence_ = print_frequence; }
+  Design Search();
+  Design IncrementalSearch(Design design);
  private:
+  void InitDefaultParam();
   void ShuffleM(std::vector<std::pair<int, int>>& pair_list, int m);
  private:
+  int n_;
+  int k_;
+  int m_col_;
+  int j_col_pair_;
+  double alpha1_;
+  double alpha2_;
+  double alpha3_;
+  double w_;
+  int iterate_cnt_;
   std::mt19937 rng_;
+  int print_frequence_;
 };
 
-Solver::Solver(int seed) {
-  rng_.seed(seed);
+Searcher::Searcher(int n, int k) : n_(n), k_(k) {
+  InitDefaultParam();
 }
 
-Design Solver::Solve(int n, int k, double w, int limit_cnt) {
-  Design design(n, k, rng_());
-  return Solve(n, k, w, limit_cnt, design);
+void Searcher::InitDefaultParam() {
+  int n_e = n_ * (n_ - 1) / 2;
+  j_col_pair_ = std::min(50, (n_e + 4) / 5);
+  m_col_ = k_;
+  alpha1_ = 0.8;
+  alpha2_ = 0.9;
+  alpha3_ = 0.7;
+  rng_.seed(0);
+  w_ = 0.5;
+  iterate_cnt_ = 1000;
+  print_frequence_ = 100;
 }
 
-Design Solver::Solve(int n, int k, double w, int limit_cnt, Design design) {
-  const int kPrintRatio = 100;
-  int n_e = n * (n - 1) / 2;
-  int J = std::min(50, (n_e + 4) / 5);
-  int M = k;
-  double a1 = 0.8;
-  double a2 = 0.9;
-  double a3 = 0.7;
-  int explore_dir = 0;
+Design Searcher::Search() {
+  Design design(n_, k_, rng_());
+  return IncrementalSearch(design);
+}
+
+Design Searcher::IncrementalSearch(Design design) {
+  assert(design.GetN() == n_ && design.GetK() == k_);
   std::uniform_real_distribution<double> uniform_dis(0.0, std::nextafter(1.0, 1.1));
-  int iterator_cnt = 0;
-  double bst_corr = design.GetMaxAbsCorr();
-  double bst_val = design.GetCritVal(w, bst_corr, design.GetPhiP());
-  Design::VecInt2D bst_design = design.GetDesign();
-  double T_h = 0.005 * bst_val;
+  int cnt = 0;
+  double opt_corr = design.GetMaxAbsCorr();
+  double opt_val = design.GetCritVal(w_, opt_corr, design.GetPhiP());
+  Design::VecInt2D opt_design = design.GetDesign();
+  double T_h = 0.005 * opt_val;
   std::vector<std::pair<int, int>> pair_list;
-  pair_list.reserve(n * (n - 1) / 2);
-  for (int i = 0; i < n; ++i) {
-    for (int j = i + 1; j < n; ++j) {
+  pair_list.reserve(n_ * (n_ - 1) / 2);
+  for (int i = 0; i < n_; ++i) {
+    for (int j = i + 1; j < n_; ++j) {
       pair_list.emplace_back(i, j);
     }
   }
-  double cur_val = bst_val;
-  double cur_corr = bst_corr;
-  while (iterator_cnt < limit_cnt) {
-    if (iterator_cnt % kPrintRatio == 0) {
-      std::cerr << "Iterator: " << iterator_cnt 
-        << ", Val: " << cur_val 
-        << ", PhiP: " << design.GetPhiP()
-        << ", RhoMax: " << cur_corr << std::endl;
+  double cur_val = opt_val;
+  double cur_corr = opt_corr;
+  int max_no_imp_cnt = 0;
+  int explore_dir = 0;
+  auto StopSearch = [this, &cnt, &max_no_imp_cnt]() -> bool {
+    return iterate_cnt_ >= 0 && cnt >= iterate_cnt_ || max_no_imp_cnt >= 1000;
+  };
+  auto PrintLog = [this, &cnt, &cur_val, &cur_corr, &design]() -> void {
+    std::cerr << "Iteration: " << cnt
+      << ", Val: " << cur_val 
+      << ", PhiP: " << design.GetPhiP()
+      << ", RhoMax: " << cur_corr << std::endl;
+  };
+  while (true) {
+    bool stop = StopSearch();
+    if (stop || cnt % print_frequence_ == 0) {
+      PrintLog();
+      if (stop) break;
     }
-    ++iterator_cnt;
-    double old_val = bst_val;
+    ++cnt;
+
+    double old_val = opt_val;
     int n_acpt = 0;
     int n_imp = 0;
-    for (int i = 0; i < M; ++i) {
-      int col = i % k;
-      double inner_bst_val = __DBL_MAX__;
-      double inner_bst_corr = __DBL_MAX__;
-      ShuffleM(pair_list, J);
-      int bst_r1 = 0;
-      int bst_r2 = 0;
+    for (int i = 0; i < m_col_; ++i) {
+      int col = i % k_;
+      double inner_opt_val = __DBL_MAX__;
+      double inner_opt_corr = __DBL_MAX__;
+      ShuffleM(pair_list, j_col_pair_);
+      std::pair<int, int> opt_pair;
       double corr_except = design.GetMaxAbsCorrExcept(col);
-      for (int j = 0; j < J; ++j) {
-        int r1 = pair_list[j].first;
-        int r2 = pair_list[j].second;
-        auto tmp_ret = design.PreSwapInCol(col, r1, r2);
+      for (int j = 0; j < j_col_pair_; ++j) {
+        auto pair = pair_list[j];
+        auto tmp_ret = design.PreSwapInCol(col, pair.first, pair.second);
         double tmp_corr = std::max(corr_except, tmp_ret.first);
-        double tmp_val = design.GetCritVal(w, tmp_corr, tmp_ret.second);
-        if (tmp_val < inner_bst_val) {
-          inner_bst_val = tmp_val;
-          inner_bst_corr = tmp_corr;
-          bst_r1 = r1;
-          bst_r2 = r2;
+        double tmp_val = design.GetCritVal(w_, tmp_corr, tmp_ret.second);
+        if (tmp_val < inner_opt_val) {
+          inner_opt_val = tmp_val;
+          inner_opt_corr = tmp_corr;
+          opt_pair = pair;
         }
       }
-      if (inner_bst_val - cur_val <= T_h * uniform_dis(rng_)) {
-        design.SwapInCol(col, bst_r1, bst_r2);
+      if (inner_opt_val - cur_val <= T_h * uniform_dis(rng_)) {
+        design.SwapInCol(col, opt_pair.first, opt_pair.second);
         ++n_acpt;
-        if (inner_bst_val < bst_val) {
-          bst_val = inner_bst_val;
-          bst_design = design.GetDesign();
+        if (inner_opt_val < opt_val) {
+          opt_val = inner_opt_val;
+          opt_design = design.GetDesign();
           ++n_imp;
         }
-        cur_val = inner_bst_val;
-        cur_corr = inner_bst_corr;
+        cur_val = inner_opt_val;
+        cur_corr = inner_opt_corr;
       }
     }
-    bool flag_imp = old_val - bst_val > 0;
-    double acpt_ratio = static_cast<double>(n_acpt) / M;
-    double imp_ratio = static_cast<double>(n_imp) / M;
+
+    bool flag_imp = old_val - opt_val > 0;
+    max_no_imp_cnt = flag_imp ? 0 : max_no_imp_cnt + 1;
+    double acpt_ratio = 1.0 * n_acpt / m_col_;
+    double imp_ratio = 1.0 * n_imp / m_col_;
     if (flag_imp) {
       if (acpt_ratio > 0.1 && n_acpt > n_imp) {
-        T_h *= a1;
-      }
-      else if (acpt_ratio > 0.1 && n_acpt == n_imp) {
+        T_h *= alpha1_;
+      } else if (acpt_ratio > 0.1 && n_acpt == n_imp) {
         ;
+      } else {
+        T_h /= alpha1_;
       }
-      else {
-        T_h /= a1;
-      }
-    }
-    else {
+    } else {
       if (acpt_ratio < 0.1) {
-        T_h /= a3;
+        T_h /= alpha3_;
         explore_dir = 1;
-      }
-      else if (acpt_ratio > 0.8) {
-        T_h *= a2;
+      } else if (acpt_ratio > 0.8) {
+        T_h *= alpha2_;
         explore_dir = -1;
-      }
-      else if (explore_dir == 1) {
-        T_h /= a3;
-      }
-      else if (explore_dir == -1) {
-        T_h *= a2;
+      } else if (explore_dir == 1) {
+        T_h /= alpha3_;
+      } else if (explore_dir == -1) {
+        T_h *= alpha2_;
       }
     }
   }
-  return Design(bst_design);
+  return Design(opt_design);
 }
 
-void Solver::ShuffleM(std::vector<std::pair<int, int>>& pair_list, int m) {
+void Searcher::ShuffleM(std::vector<std::pair<int, int>>& pair_list, int m) {
   int n = pair_list.size();
   assert(m <= n);
   for (int i = 0; i < m; ++i) {
@@ -402,12 +433,14 @@ int main(int argc, char** argv) {
   int k = std::stoi(argv[2]);
   double w = std::stod(argv[3]);
   int cnt = std::stoi(argv[4]);
+  auto searcher = ESEJin2005::Searcher(n, k);
+  searcher.SetW(w);
+  searcher.SetIterateCnt(cnt);
   if (argc > 5) {
-    auto init_design = ESEJin2005::Design::ReadDesign(argv[5]);
-    ESEJin2005::Solver().Solve(n, k, w, cnt, init_design).Display();
-  }
-  else {
-    ESEJin2005::Solver().Solve(n, k, w, cnt).Display();
+    auto design = ESEJin2005::Design::ReadDesign(argv[5]);
+    searcher.IncrementalSearch(design).Display();
+  } else {
+    searcher.Search().Display();
   }
   return 0;
 }
