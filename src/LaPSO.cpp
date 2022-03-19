1663830d32d3e700480d510327c3abc19b0e5007
DougZheng <739168993@qq.com>
2022-03-19 16:45:21 +0800

    Add LaPSO

---
+++ src/LaPSO.cpp
@@ -0,0 +1,18 @@
+#include "LaPSO.h"
+
+int main(int argc, char** argv) {
+  if (argc < 4) {
+    std::cout << "Usage: ${exe} ${n} ${k} ${w} ${cnt}" << std::endl;
+    return 1;
+  }
+  // ./LaPSO 20 8 0.5 1000 >LHD
+  int n = std::stoi(argv[1]);
+  int k = std::stoi(argv[2]);
+  double w = std::stod(argv[3]);
+  int cnt = std::stoi(argv[4]);
+  auto searcher = SearchingAlgorithm::LaPSO(n, k);
+  searcher.SetW(w);
+  searcher.SetIterateCnt(cnt);
+  searcher.Search().Display();
+  return 0;
+}
